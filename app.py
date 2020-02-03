8da3f76f88407fe6f33fb392d3141b15d3d20faf
Natsuki <haseganats@myvuw.ac.nz>
2020-02-03 16:03:45 +1300

    new form

--- app.py
+++ app.py
@@ -86,5 +86,18 @@ def form():
         return render_template('form.html')
 
 
+@app.route('/form2', methods=['POST', 'GET'])
+def form2():
+    # if request.method == 'POST':
+    #
+    #     return render_template('form2.html',
+    #                            text=request.form['text_input'],
+    #                            option_1=request.form['radio_set1'],
+    #                            option_2=request.form['radio_set2'])
+    #
+    # else:
+    return render_template('form2.html')
+
+
 if __name__ == '__main__':
     app.run()
