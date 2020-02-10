7e575415240005d63efa8bea208ac1a324d77aa1
Peggy Nobes <nobesmarg@myvuw.ac.nz>
2020-02-10 16:30:05 +1300

    filter user only ripples for moderation

--- static/scripts/review.js
+++ static/scripts/review.js
@@ -9,15 +9,17 @@ function getRippleDetails() {
       // let message = "received source and mod tagged:\n";
       // let i = 0;
       Object.keys(ripples).forEach(function (key) {
-          let ripple = new user_response();
-          ripple.source = ripples[key].source;
-          ripple.message = ripples[key].message;
-          ripple.date = new Date(ripples[key].date);
-          ripple.id = ripples[key].ripple_id;
-          ripple.moderationflag = ripples[key].moderate;
-          ripple.userRating = ripples[key].user_rating;
-          ripple.orgRating = ripples[key].org_rating;
-          ripple_objs.push(ripple);
+          if(ripples[key].source == "user") {
+              let ripple = new user_response();
+              ripple.source = ripples[key].source;
+              ripple.message = ripples[key].message;
+              ripple.date = new Date(ripples[key].date);
+              ripple.id = ripples[key].ripple_id;
+              ripple.moderationflag = ripples[key].moderate;
+              ripple.userRating = ripples[key].user_rating;
+              ripple.orgRating = ripples[key].org_rating;
+              ripple_objs.push(ripple);
+          }
           // message = message + rippleDateSpan(ripple.date) + ", " + ripples[key].message + "\n" + ripples[key].moderate + "\n";
           // i++;
       })
